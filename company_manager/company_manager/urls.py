1cc898c6f6baba6391f018b54658723fc5406ac6
Jiří Pešík <jiri.pesik@outlook.cz>
2022-04-11 19:57:24 +0200

    Změny během 3. lekce

--- company_manager/company_manager/urls.py
+++ company_manager/company_manager/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("crm.urls"))
+    path("", include("crm.urls")),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
--- company_manager/crm/views.py
+++ company_manager/crm/views.py
@@ -1,17 +1,19 @@
 from django.views.generic import CreateView, ListView, TemplateView
 import crm.models as models
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
-class CompanyCreateView(CreateView):
+class CompanyCreateView(LoginRequiredMixin, CreateView):
     model = models.Company
     template_name = "company/create_company.html"
     fields = ["name", "status", "phone_number", "email", "identification_number"]
     success_url = reverse_lazy("index")
 
-class OpportunityCreateView(CreateView):
+class OpportunityCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = "crm.add_opportunity"
     model = models.Opportunity
     template_name = "company/create_company.html"
     fields = ["company", "sales_manager", "description", "status", "value"]
