c9545b298b4822d7c5b63bd0dde8ebaf7c24c59b
Jiří Pešík <jiri.pesik@outlook.cz>
2022-05-15 17:16:14 +0200

    Heroku

--- company_manager/company_manager/settings.py
+++ company_manager/company_manager/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 
-import django_heroku
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +140,7 @@ LANGUAGES = [
     ["cs", "Czech"],
     ["en", "English"]
 ]
+
+import django_heroku
+
+django_heroku.settings(locals())
